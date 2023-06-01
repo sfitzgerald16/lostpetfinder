a037f8f131d55969b940a4cdb519f54e0ae82924
Shannon FitzGerald <misfitz616@gmail.com>
2023-06-01 15:55:15 -0700

    add serializers, views and urls

--- lostpetfinder/settings.py
+++ lostpetfinder/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'phonenumber_field',
-    'lostpetfinder.pets',
+    'rest_framework',
     'lostpetfinder.account',
 ]
 
